c673cdd7962a4f6e884ac2f40b9935423ec9d735
Naveen <83157625+naveenkanuri@users.noreply.github.com>
2021-09-04 23:53:08 +0530

    3. Created Message Loop and custom WndProc

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,5 +1,17 @@
 #include <Windows.h>
 
+// our custom WindowProcedure to handle windows' messages
+LRESULT CALLBACK WndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
+{
+	switch( msg )
+	{
+	case WM_CLOSE:
+		PostQuitMessage( 69 );
+		break;
+	}
+	return DefWindowProc( hWnd, msg, wParam, lParam );
+}
+
 int CALLBACK WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -11,7 +23,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof( wc );
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -40,6 +52,20 @@ int CALLBACK WinMain(
 
 	// show Window
 	ShowWindow( hWnd, SW_SHOW );
-	while( true );
-	return 0;
+
+	// message pump
+	MSG msg;
+	BOOL gResult;
+	while( gResult = ( GetMessage( &msg, nullptr, 0, 0 ) ) )
+	{
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
+	}
+
+	if( gResult == -1 )
+	{
+		return -1;
+	}
+	
+	return msg.wParam;
 }
