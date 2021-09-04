0e73463215ad1d8dbf3e8c93cc2e0d27aed34c57
Naveen <83157625+naveenkanuri@users.noreply.github.com>
2021-09-05 02:47:18 +0530

    5. WM_CHAR/Mouse

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include "WindowsMessageMap.h"
+#include <sstream>
 
 // our custom WindowProcedure to handle windows' messages
 LRESULT CALLBACK WndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
@@ -16,11 +17,30 @@ LRESULT CALLBACK WndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
 		{
 			SetWindowText( hWnd, L"Hi Honey" );
 		}
+		else if( wParam = VK_ESCAPE ) // close on clicking Escape key
+		{
+			PostQuitMessage( 99 );
+		}
 		break;
 	case WM_KEYUP:
 		if( wParam == 'F' )
 		{
 			SetWindowText( hWnd, L"How are you" );
+		}
+		break;
+	case WM_CHAR:
+		{
+			static std::wstring title;
+			title.push_back( ( char ) wParam );
+			SetWindowText( hWnd, title.c_str() );
+		}
+		break;
+	case WM_LBUTTONDOWN:
+		{
+			const POINTS pt = MAKEPOINTS( lParam );
+			std::wostringstream woss;
+			woss << "(" << pt.x << "," << pt.y << ")";
+			SetWindowText( hWnd, woss.str().c_str( ) );
 		}
 		break;
 	}
