44f14380d6242193254b22b030d949d28df0f73c
Naveen <83157625+naveenkanuri@users.noreply.github.com>
2021-09-09 04:46:50 +0530

    16,17. First Triangle

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -1,10 +1,12 @@
 #include "Graphics.h"
 #include "dxerr.h"
 #include <sstream>
+#include <d3dcompiler.h>
 
 namespace wrl = Microsoft::WRL;
 
 #pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "D3DCompiler.lib")
 
 #define GFX_EXCEPT_NOINFO(hr) Graphics::HrException(__LINE__, __FILE__, (hr))
 #define GFX_THROW_NOINFO(hrcall) if (FAILED(hr=(hrcall))) throw Graphics::HrException(__LINE__, __FILE__, (hr))
@@ -13,10 +15,12 @@ namespace wrl = Microsoft::WRL;
 #define GFX_EXCEPT(hr) Graphics::HrException(__LINE__, __FILE__, (hr), infoManager.GetMessages())
 #define GFX_THROW_INFO(hrcall) infoManager.Set(); if(FAILED(hr=hrcall)) throw GFX_EXCEPT(hr)
 #define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__, __FILE__, (hr), infoManager.GetMessages())
+#define GFX_THROW_INFO_ONLY(call) infoManager.Set(); (call); {auto v = infoManager.GetMessages(); if(!v.empty()) {throw Graphics::InfoException(__LINE__, __FILE__, v);}}
 #else
 #define GFX_EXCEPT(hr) Graphics::HrException(__LINE__, __FILE__, (hr))
 #define GFX_THROW_INFO(hrcall) GFX_THROW_NOINFO(hrcall)
 #define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__, __FILE__, (hr))
+#define GFX_THROW_INFO_ONLY(call) (call)
 #endif
 
 Graphics::Graphics( HWND hWnd )
@@ -95,6 +99,107 @@ void Graphics::ClearBuffer( float red, float green, float blue ) noexcept
 	pContext->ClearRenderTargetView( pTarget.Get(), color );
 }
 
+void Graphics::DrawTestTriangle()
+{
+	namespace wrl = Microsoft::WRL;
+	HRESULT hr;
+
+	struct Vertex
+	{
+		float x;
+		float y;
+	};
+
+	// create vertex buffer (1 2d triangle at the center of the screen)
+	const Vertex vertices[] =
+	{
+		{0.0f, 0.5f},
+		{0.5f, -0.5f},
+		{-0.5f, -0.5f},
+	};
+
+	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
+	D3D11_BUFFER_DESC bd = {};
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.CPUAccessFlags = 0u;
+	bd.MiscFlags = 0u;
+	bd.ByteWidth = sizeof( vertices );
+	bd.StructureByteStride = sizeof( Vertex );
+
+	D3D11_SUBRESOURCE_DATA sd = {};
+	sd.pSysMem = vertices;
+
+	GFX_THROW_INFO( pDevice->CreateBuffer( &bd, &sd, &pVertexBuffer ) );
+
+	wrl::ComPtr<ID3DBlob> pBlob;
+	//create pixel shader
+	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
+	GFX_THROW_INFO( D3DReadFileToBlob( L"PixelShader.cso", &pBlob ) );
+	GFX_THROW_INFO( pDevice->CreatePixelShader(
+		pBlob->GetBufferPointer()
+		, pBlob->GetBufferSize()
+		, nullptr
+		, &pPixelShader
+	) );
+
+	//bind pixel shader
+	pContext->PSSetShader( pPixelShader.Get(), nullptr, 0u );
+
+	// bind vertex buffer to the pipeline
+	const UINT stride = sizeof( Vertex );
+	const UINT offset = 0u;
+	pContext->IASetVertexBuffers( 0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset );
+	
+	//create vertex shader
+	wrl::ComPtr<ID3D11VertexShader> pVertexShader;
+	GFX_THROW_INFO( D3DReadFileToBlob( L"VertexShader.cso", &pBlob ) );
+	GFX_THROW_INFO( pDevice->CreateVertexShader(
+		pBlob->GetBufferPointer()
+		, pBlob->GetBufferSize() 
+		, nullptr
+		, &pVertexShader
+	));
+
+	// input (vertex) layout (2d position only)
+	wrl::ComPtr<ID3D11InputLayout> pInputLayout;
+	const D3D11_INPUT_ELEMENT_DESC ied[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0},
+	};
+	GFX_THROW_INFO( pDevice->CreateInputLayout(
+		ied
+		, ( UINT ) std::size( ied )
+		, pBlob->GetBufferPointer()
+		, pBlob->GetBufferSize()
+		, &pInputLayout
+	) );
+
+	//bind input layout
+	pContext->IASetInputLayout( pInputLayout.Get() );
+
+	//bind vertex shader
+	pContext->VSSetShader( pVertexShader.Get(), nullptr, 0u );
+
+	//bind render target
+	pContext->OMSetRenderTargets( 1u, pTarget.GetAddressOf(), nullptr );
+
+	//set primitive topology to triangle list (group of 3 vertices)
+	pContext->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
+
+	//configure viewport
+	D3D11_VIEWPORT vp;
+	vp.Width = 800;
+	vp.Height = 600;
+	vp.MinDepth = 0;
+	vp.MaxDepth = 1;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	pContext->RSSetViewports( 1u, &vp );
+
+	GFX_THROW_INFO_ONLY( pContext->Draw( (UINT)std::size(vertices), 0u ) );
+}
+
 Graphics::HrException::HrException( int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs ) noexcept
 	:Exception( line, file )
 	, hr( hr )
@@ -155,4 +260,38 @@ std::string Graphics::HrException::GetErrorInfo() const noexcept
 const char* Graphics::DeviceRemovedException::GetType() const noexcept
 {
 	return "Honey Graphics Exception [Device Removed] (DXGI_ERROR_DEVICE_REMOVED)";
+}
+
+Graphics::InfoException::InfoException( int line, const char* file, std::vector<std::string> infoMsgs /*= {} */ ) noexcept
+	:Exception(line, file)
+{
+	//join all messages with newlines into a single string
+	for( const auto& m : infoMsgs )
+	{
+		info += m;
+		info.push_back( '\n' );
+	}
+	//remove final new line if exists
+	if( !info.empty() )
+		info.pop_back();
+}
+
+const char* Graphics::InfoException::what() const noexcept
+{
+	std::ostringstream oss;
+	oss << GetType() << std::endl
+		<< "\n[Error Info] " << GetErrorInfo() << std::endl << std::endl;
+	oss << GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+}
+
+const char* Graphics::InfoException::GetType() const noexcept
+{
+	return "Honey Graphics Info Exception";
+}
+
+std::string Graphics::InfoException::GetErrorInfo() const noexcept
+{
+	return info;
 }
